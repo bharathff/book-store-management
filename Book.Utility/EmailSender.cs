90774c6222a203e84219e0ac1c7f237c46dd462b
Bharath M <bharath.m2020ece@sece.ac.in>
2024-06-01 12:04:26 +0530

    online book sale

---
+++ Book.Utility/EmailSender.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Threading.Tasks;
+
+namespace Book.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            // Your email sending logic goes here
+            // For simplicity, returning a completed task
+            return Task.CompletedTask;
+        }
+    }
+}
